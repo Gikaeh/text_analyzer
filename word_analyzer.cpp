9bc281857e77815507657b75f7be7f2e6c60a585
Gikaeh <35640012+Gikaeh@users.noreply.github.com>
2021-11-18 13:40:56 -0800

    Add files via upload

---
+++ word_analyzer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include "word_analyzer.h"
+using namespace std;
+
+int countLetters(string s) { //string s designates all string information into char s
+    int res = 0;
+    for (char c : s) { //char c:s makes the char c hold all information contained in string s
+        if (isalpha (c)) //checks all characters to see if they're letter in order to count
+        ++res; //add the found number to the res variable before outputting (26+res(0)=26)
+    }
+    return res; //puts out res variable
+}
+int countLowerCase(string s) {
+    int res = 0;
+    for (char c : s) {
+        if (islower (c))
+        ++res
+    }
+    return res;
+}
+int countUpperCase(string s) {
+    int res = 0;
+    for (char c : s) {
+        if (isupper (c))
+        ++res;
+    }
+    return res;
+}
+int countVowels(string s) {
+    int res = 0;
+    for (char c : s) {
+        c = tolower (c);
+        switch (c) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                ++res;
+                break;
+        }
+    }
+    return res;
+}
---
+++ word_analyzer.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int countLetters(string);
+int countLowerCase(string);
+int countUpperCase(string);
+int countVowels(string);
